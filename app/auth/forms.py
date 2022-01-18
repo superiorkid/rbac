21c6364540a13c58dec122ef258b7f97d1a049a0
superiorkid <bayiPetani@protonmail.com>
2022-01-18 12:17:20 +0800

    user form login

---
+++ app/auth/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class LoginForm(FlaskForm):
+  email = StringField('Email', validators=[DataRequired(), Email(), Length(1, 64)])
+  password = PasswordField('Password', validators=[DataRequired()])
+  remember_me = BooleanField('Remember Me')
+  submit = SubmitField('Submit')
