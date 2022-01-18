ebcd07668fbbfbf217937a338125b527c968703c
superiorkid <bayiPetani@protonmail.com>
2022-01-18 12:16:28 +0800

    improve main page

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,11 @@
-from flask import jsonify
+from flask import render_template
 from . import main
-
+from flask_login import login_required, current_user
 
 @main.route('/')
+@login_required
 def index():
-  return jsonify({'status': 'OK'})
+
+  user = {'User': current_user.username }
+
+  return render_template('main/index.html', user=user)
