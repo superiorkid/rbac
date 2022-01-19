5dcb975a74f75ba0493941e3d0d479337a1892f6
superiorkid <bayiPetani@protonmail.com>
2022-01-19 08:50:17 +0800

    update pada label form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,5 +6,5 @@ from wtforms.validators import DataRequired, Email, Length
 class LoginForm(FlaskForm):
   email = StringField('Email', validators=[DataRequired(), Email(), Length(1, 64)])
   password = PasswordField('Password', validators=[DataRequired()])
-  remember_me = BooleanField('Remember Me')
-  submit = SubmitField('Submit')
+  remember_me = BooleanField('Keep me logged in')
+  submit = SubmitField('Log In')
