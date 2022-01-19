de0c595a58e961d8df949484534e22b6df025d51
superiorkid <bayiPetani@protonmail.com>
2022-01-19 12:44:49 +0800

    sedikit perbaikan

--- app/main/views.py
+++ app/main/views.py
@@ -5,4 +5,4 @@ from flask_login import login_required, current_user
 @main.route('/')
 @login_required
 def index():
-  return render_template('main/index.html')
+  return render_template('main/index.html', title="Index")
