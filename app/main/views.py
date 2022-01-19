5fda813c4f139b3857cf6352e3c8598dfe5efca3
superiorkid <bayiPetani@protonmail.com>
2022-01-19 19:36:58 +0800

    route untuk membedakan role antara admin dan user biasa

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,24 @@
 from flask import render_template
 from . import main
 from flask_login import login_required, current_user
+from ..decorators import admin_required, permission_required
+from ..models import Permission
 
 @main.route('/')
 @login_required
 def index():
   return render_template('main/index.html', title="Index")
+
+
+@main.route('/admin')
+@login_required
+@admin_required
+def for_admin():
+  return 'for administrator'
+
+
+@main.route('/users')
+@login_required
+@permission_required(Permission.COMMENT)
+def for_user():
+  return 'for USER'
