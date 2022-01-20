b2116a35cf76e23ce0ad739f62b68a314f87c510
superiorkid <bayiPetani@protonmail.com>
2022-01-20 11:49:21 +0800

    initial commit

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config(object):
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard-to-guess'
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://superiorkid:root@localhost/rbac'
   SQLALCHEMY_TRACK_MODIFICATIONS = False
-  FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+  IS_ADMIN = os.environ.get('IS_ADMIN')
