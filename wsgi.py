aabefcd48bf6b5c9fc68f6546adad8d06b0d70e1
superiorkid <bayiPetani@protonmail.com>
2022-01-23 14:54:56 +0800

    tambahan anonymoususer

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 from app import create_app, db
-from app.models import User, Role, Permission
+from app.models import User, Role, Permission, AnonymouseUser
 from dotenv import load_dotenv
 
 load_dotenv('.env')
@@ -8,4 +8,4 @@ app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-  return {'db': db, 'User': User, 'Role': Role, 'Permission': Permission}
+  return {'db': db, 'User': User, 'Role': Role, 'Permission': Permission, 'AnonymousUser': AnonymouseUser}
